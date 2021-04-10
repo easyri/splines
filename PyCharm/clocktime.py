85b294f6ed9f56aed10340716a402db628c47249
childhoodisend <morozvktv@gmail.com>
2021-04-10 10:32:02 +0300

    [*] add Adamal to clocktime.py

--- PyCharm/clocktime.py
+++ PyCharm/clocktime.py
@@ -9,4 +9,21 @@ import numpy as np
 from show import amdal
 
 
-time = min(timeit.Timer(cubic_spline).repeat(repeat=10))
+def clocktime(repeat=2):
+    time_q = np.zeros(4)
+    time_quadratic = min(timeit.Timer(quadratic_spline).repeat(repeat=repeat))
+    time_quadratic_proc = np.array(4)
+    for i in range(2, 5):
+        time_quadratic_proc[i - 1] = min(timeit.Timer(process_quadratic(i)).repeat(repeat=repeat))
+    time_q[0] = time_quadratic
+    time_q += time_quadratic_proc
+    amdal(time_q)
+
+    time_c = np.zeros(4)
+    time_cubic = min(timeit.Timer(cubic_spline).repeat(repeat=repeat))
+    time_cubic_proc = np.array(3)
+    for i in range(2, 5):
+        time_cubic_proc[i - 2] = min(timeit.Timer(process_cubic(i)).repeat(repeat=repeat))
+    time_c[0] = time_cubic
+    time_c += time_cubic_proc
+    amdal(time_c)
