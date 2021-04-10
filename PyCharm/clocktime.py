ca8ff219ac431248cb9fe4c79a061d58b5195ee9
childhoodisend <morozvktv@gmail.com>
2021-04-10 11:58:50 +0300

    [*] update clocktime.py

--- PyCharm/clocktime.py
+++ PyCharm/clocktime.py
@@ -9,21 +9,23 @@ import numpy as np
 from show import amdal
 
 
-def clocktime(repeat=2, proc=4):
-    time_q = np.zeros(proc)
-    time_quadratic = min(timeit.Timer(quadratic_spline).repeat(repeat=repeat))
-    time_quadratic_proc = np.array(proc)
-    for i in range(1, proc):
-        time_quadratic_proc[i] = min(timeit.Timer(process_quadratic(i + 1)).repeat(repeat=repeat))
+def clocktime(num=1, proc=2):
+    time_q = np.zeros(proc + 1)
+    time_quadratic = timeit.timeit(quadratic_spline, number=num)/num
+    time_quadratic_proc = np.zeros(proc + 1)
+    for i in range(1, proc + 1):
+        time_quadratic_proc[i] = timeit.Timer(lambda: process_quadratic(i + 1)).timeit(number=num) / num
     time_q[0] = time_quadratic  # 1 thread script
     time_q += time_quadratic_proc  # 2, 3, 4 ... process script
-    amdal(time_q)
+    print(time_q)
+    amdal(time_q, 'amdal_qua')
 
-    time_c = np.zeros(proc)
-    time_cubic = min(timeit.Timer(cubic_spline).repeat(repeat=repeat))
-    time_cubic_proc = np.array(proc)
-    for i in range(1, proc):
-        time_cubic_proc[i] = min(timeit.Timer(process_cubic(i + 1)).repeat(repeat=repeat))
+    time_c = np.zeros(proc + 1)
+    time_cubic = timeit.timeit(cubic_spline, number=num) / num
+    time_cubic_proc = np.zeros(proc + 1)
+    for i in range(1, proc + 1):
+        time_cubic_proc[i] = timeit.Timer(lambda: process_cubic(i + 1)).timeit(number=num) / num
     time_c[0] = time_cubic
     time_c += time_cubic_proc
-    amdal(time_c)
+    print(time_c)
+    amdal(time_c, 'amdal_cub')
